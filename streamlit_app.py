58d199bf2a7d70f438d16f1e73a7a52a1191b896
GOKULBOOPATHIACC <143089522+GOKULBOOPATHIACC@users.noreply.github.com>
2023-08-24 18:25:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title(' My parents New Healthy Diner ')
-streamlit.header(' Breakfast Menu ')
-streamlit.text(' Omega 3 & strawberry Oatmeal ')
-streamlit.text(' Kale, Spinach and Rocket smoothie ')
-streamlit.text(' Hard boiled free range egg ')
+streamlit.header(' 🥣Breakfast Menu ')
+streamlit.text(' 🥗Omega 3 & strawberry Oatmeal ')
+streamlit.text(' 🥑🍞Kale, Spinach and Rocket smoothie ')
+streamlit.text(' 🐔Hard boiled free range egg ')
