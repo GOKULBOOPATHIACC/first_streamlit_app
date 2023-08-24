f5163de10fcb17425507b26917a592df018596b7
GOKULBOOPATHIACC <143089522+GOKULBOOPATHIACC@users.noreply.github.com>
2023-08-24 18:20:20 +0530

    Update streamlit_app.py
    Added title, header and text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title(' My parents New Healthy Diner ')
+streamlit.Header(' Breakfast Menu ')
+streamlit.text(' Omega 3 & strawberry Oatmeal ')
+streamlit.title(' Kale, spinach and rocket smoothie ')
+streamlit.title(' Hard boiled free range egg ')
